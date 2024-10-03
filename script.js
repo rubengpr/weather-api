c13c1c0be8e2dd228c3882da2d6b458ddd67a0eb
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-10-03 03:09:34 +0200

    Add v1 icons

--- script.js
+++ script.js
@@ -8,13 +8,43 @@ async function getWeatherData(event) {
 };
 
 function displayInfo(rawData) {
+    const outputBox = document.getElementById("output-box");
+
+    outputBox.style.display = "flex";
+    
+    const icon = document.getElementById("icon");
     const description = document.getElementById("description");
     const temperature = document.getElementById("temperature");
     const humidity = document.getElementById("humidity");
 
     description.textContent = rawData.description;
-    temperature.textContent = rawData.days[0].temp;
-    humidity.textContent = rawData.days[0].humidity;
+    temperature.textContent = "Temperature: " + rawData.days[0].temp;
+    humidity.textContent = "Humidity: " + rawData.days[0].humidity;
+
+    let iconWeather = rawData.days[0].icon;
+
+    switch (iconWeather) {
+        case "clear-day":
+            icon.src = "assets/sunny.svg"
+            break;
+        case "clear-night":
+            icon.src = "assets/halfmoon.svg"
+            break;
+        case "cloudy":
+            icon.src = "assets/cloudy.svg"
+            break;
+        case "thunder-showers-day":
+            icon.src = "assets/thunderstorm.svg"
+            break;
+        case "partly-cloudy-night":
+            icon.src = "assets/halfmoon.svg"
+            break;
+        case "partly-cloudy-day":
+            icon.src = "assets/cloudy.svg"
+            break;
+    }
+
+
 }
 
 const userInput = document.getElementById("input").addEventListener("change", getWeatherData)
