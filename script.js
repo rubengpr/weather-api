243fc49b9719d6e42852ddc494c37079bc3b8d26
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-10-06 21:20:03 +0200

    Add icons and API fetching

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-async function getWeatherData(event) {
-    let city = event.target.value
+const input = document.getElementById("city-input");
+
+window.onload = () => {
+    input.value = "Barcelona";
+    getWeatherData();
+}
+
+async function getWeatherData() {
+    let city = input.value
     let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=SBT2PTJB3ASMEVTUST7UJU9GH`, {
         mode: 'cors'
       });
@@ -8,43 +15,106 @@ async function getWeatherData(event) {
 };
 
 function displayInfo(rawData) {
-    const outputBox = document.getElementById("output-box");
 
-    outputBox.style.display = "flex";
-    
-    const icon = document.getElementById("icon");
-    const description = document.getElementById("description");
-    const temperature = document.getElementById("temperature");
+    const mainTemp = document.getElementById("temperature");
+    mainTemp.textContent = rawData.currentConditions.temp;
+
+    const mainDesc = document.getElementById("short-description");
+    mainDesc.textContent = rawData.currentConditions.conditions;
+
+    const minTemp = document.getElementById("min-temperature-value");
+    const maxTemp = document.getElementById("max-temperature-value");
+
+    const chanceRain = document.getElementById("chance-rain");
+    chanceRain.textContent = rawData.currentConditions.precipprob + "%";
+
     const humidity = document.getElementById("humidity");
+    humidity.textContent = rawData.currentConditions.humidity + "%";
 
-    description.textContent = rawData.description;
-    temperature.textContent = "Temperature: " + rawData.days[0].temp;
-    humidity.textContent = "Humidity: " + rawData.days[0].humidity;
+    const wind = document.getElementById("wind");
+    wind.textContent = rawData.currentConditions.windspeed + " km/h";
 
+    const uvIndex = document.getElementById("uv-index");
+    uvIndex.textContent = rawData.currentConditions.uvindex;
+
+    const sunrise = document.getElementById("sunrise");
+    sunrise.textContent = rawData.currentConditions.sunrise;
+
+    const sunset = document.getElementById("sunset");
+    sunset.textContent = rawData.currentConditions.sunset;
+
+    const hourlyForecastTime = document.getElementsByClassName("hourly-time");
+    const timeArray = Array.from(hourlyForecastTime);
+    
+    timeArray.forEach(function(time, i) {
+        time.textContent = rawData.days[i].hours[i].datetime + " pm";
+    }
+);
+
+    const hourlyForecastIcon = document.getElementsByClassName("hourly-icon");
+    const iconArray = Array.from(hourlyForecastIcon);
+
+    iconArray.forEach(function(hourIcon, i) {
+        let iconOutput = rawData.days[i].hours[i].icon
+        assignIcon(iconOutput, hourIcon)
+    }
+);
+
+    const mainIcon = document.getElementById("main-icon");
     let iconWeather = rawData.days[0].icon;
+    assignIcon(iconWeather, mainIcon);
 
-    switch (iconWeather) {
-        case "clear-day":
-            icon.src = "assets/sunny.svg"
-            break;
-        case "clear-night":
-            icon.src = "assets/halfmoon.svg"
-            break;
+function assignIcon(outputName, icon) {
+    switch (outputName) {
+        case "snow":
+            icon.src = "assets/snow.svg"
+            break
+        case "rain":
+            icon.src = "assets/rain.svg"
+            break
+        case "fog":
+            icon.src = "assets/fog.svg"
+            break
+        case "wind":
+            icon.src = "assets/wind.svg"
+            break
         case "cloudy":
             icon.src = "assets/cloudy.svg"
-            break;
-        case "thunder-showers-day":
-            icon.src = "assets/thunderstorm.svg"
-            break;
-        case "partly-cloudy-night":
-            icon.src = "assets/halfmoon.svg"
-            break;
+            break
         case "partly-cloudy-day":
-            icon.src = "assets/cloudy.svg"
-            break;
-    }
+            icon.src = "assets/partly-cloudy-day.svg"
+            break
+        case "partly-cloudy-night":
+            icon.src = "assets/partly-cloudy-night.svg"
+            break
+        case "clear-day":
+            icon.src = "assets/clear-day.svg"
+            break
+        case "clear-night":
+            icon.src = "assets/clear-night.svg"
+            break
+        case "snow-showers-night":
+            icon.src = "assets/extreme-night-snow.svg"
+            break
+        case "thunder-rain":
+            icon.src = "assets/thunderstorms-rain"
+            break
+        case "thunder-showers-day":
+            icon.src = "assets/thunderstorms-day-rain.svg"
+            break
+        case "thunder-showers-night":
+            icon.src = "assets/thunderstorms-night-rain.svg"
+            break
+        case "showers-day":
+            icon.src = "assets/rain.svg"
+            break
+        case "showers-night":
+            icon.src = "assets/rain.svg"
+            break
+        }
+    };
 
 
-}
+};
 
-const userInput = document.getElementById("city-input").addEventListener("change", getWeatherData)
+const userInput = input.addEventListener("change", getWeatherData)
